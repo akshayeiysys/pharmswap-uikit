71871553392aeeb1335f01453d0229240423642a
unknown <akshayk@eiysys.in>
2021-03-17 14:16:42 +0530

    image

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -6,10 +6,10 @@ import { ImageProps } from "./types";
 
 const StyledImage = styled.img`
   position: absolute;
-  top: 0;
+  top: -90px;
   left: 0;
   width: 100%;
-  height: 100%;
+
   max-width: 100%;
 `;
 
